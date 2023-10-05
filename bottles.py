62d277d3fe47225c5979a0f00741d8cb1cc8a3ac
Joshua Minchew <jminchew97@gmail.com>
2023-10-05 15:01:30 -0500

    finished

--- bottles.py
+++ bottles.py
@@ -1,6 +1,15 @@
 def bottle_song():
-	# write your code here!
-	pass
+	for bottle_count in reversed(range(1, 100)):
+		if bottle_count == 1:
+			
+			print("1 bottle of beer on the wall, 1 bottle of beer.")
+			print("Take one down and pass it around, no more bottles of beer on the wall.")
+			print("No more bottles of beer on the wall, no more bottles of beer.")
+			print("Go to the store and buy some more, 99 bottles of beer on the wall.")
+		else:
+			print(f"{bottle_count} bottles of beer on the wall, {bottle_count} bottles of beer.")
+			print(f"Take one down and pass it around, {bottle_count} bottles of beer on the wall.")
+
 
 bottle_song()
 
